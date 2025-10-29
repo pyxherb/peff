cd1fbbabda5408a39ee3f4add82ee4a11916bd40
pyxherb <codesbuilder@163.com>
2025-10-29 17:46:42 +0800

    Add byte order swapper.

--- peff/utils/byteord.h
+++ peff/utils/byteord.h
@@ -2,6 +2,7 @@
 #define _PEFF_UTILS_BYTEORD_H_
 
 #include "basedefs.h"
+#include <cstdint>
 
 namespace peff {
 	PEFF_UTILS_API extern bool _g_byteOrder;
@@ -11,6 +12,39 @@ namespace peff {
 	/// @return false The machine is in little-endian mode.
 	PEFF_FORCEINLINE bool getByteOrder() {
 		return _g_byteOrder;
+	}
+
+	constexpr PEFF_FORCEINLINE uint16_t swapByteOrder(uint16_t n) {
+		return (n & 0xff << 8) | (n >> 8);
+	}
+	constexpr PEFF_FORCEINLINE uint16_t swapByteOrder(int16_t n) {
+		return (n & 0xff << 8) | (n >> 8);
+	}
+	constexpr PEFF_FORCEINLINE uint32_t swapByteOrder(uint32_t n) {
+		return (n & 0xff << 24) | (n & 0xff00 << 8) | (n & 0xff0000 >> 8) | (n >> 24);
+	}
+	constexpr PEFF_FORCEINLINE uint32_t swapByteOrder(int32_t n) {
+		return (n & 0xff << 24) | (n & 0xff00 << 8) | (n & 0xff0000 >> 8) | (n >> 24);
+	}
+	constexpr PEFF_FORCEINLINE uint64_t swapByteOrder(uint64_t n) {
+		return ((n & 0xffull) << 56) |
+			   ((n & 0xff00ull) << 40) |
+			   ((n & 0xff0000ull) << 24) |
+			   ((n & 0xff000000ull) << 8) |
+			   ((n & 0xff00000000ull) >> 8) |
+			   ((n & 0xff0000000000ull) >> 24) |
+			   ((n & 0xff000000000000ull) >> 40) |
+			   ((n & 0xff00000000000000ull) >> 56);
+	}
+	constexpr PEFF_FORCEINLINE uint64_t swapByteOrder(int64_t n) {
+		return ((n & 0xffull) << 56) |
+			   ((n & 0xff00ull) << 40) |
+			   ((n & 0xff0000ull) << 24) |
+			   ((n & 0xff000000ull) << 8) |
+			   ((n & 0xff00000000ull) >> 8) |
+			   ((n & 0xff0000000000ull) >> 24) |
+			   ((n & 0xff000000000000ull) >> 40) |
+			   ((n & 0xff00000000000000ull) >> 56);
 	}
 }
 
